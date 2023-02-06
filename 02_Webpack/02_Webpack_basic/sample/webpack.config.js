2a872f17d205e6f6b12bf8609562cc30c5fb542d
hrookim <hrookim@gmail.com>
2023-02-06 23:39:03 +0900

    [feat] add file-loader

--- 02_Webpack/02_Webpack_basic/sample/webpack.config.js
+++ 02_Webpack/02_Webpack_basic/sample/webpack.config.js
@@ -1,15 +1,26 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    main: "./src/app.js",
+    main: './src/app.js',
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].js",
+    path: path.resolve('./dist'),
+    filename: '[name].js',
   },
   module: {
-    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.png$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]',
+        },
+        // use: ['file-loader'],
+      },
+    ],
   },
 };
